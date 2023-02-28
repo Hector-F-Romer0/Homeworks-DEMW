5d901307ffbf4e1467e3feba37c240ab1f8a07fe
Hector Romero <hector_fabio.romero@uao.edu.co>
2023-02-28 16:23:43 -0500

    CHL08: Realizar peticiones a la API de Breaking Bad y mostrar en pantalla las citas de sus personajes.

--- ReactChallenges/src/components/MultipleCustomHooks.jsx
+++ ReactChallenges/src/components/MultipleCustomHooks.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
+import { useCounter } from "../hooks/useCounter";
 
 const MultipleCustomHooks = () => {
-	const url = "https://api.breakingbadquotes.xyz/v1/quotes/1";
-
+	const { counter, increment } = useCounter(1);
+	const url = `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`;
 	const { data, isLoading, hasError } = useFetch(url);
 
 	useEffect(() => {
@@ -11,9 +12,36 @@ const MultipleCustomHooks = () => {
 	}, [data]);
 
 	if (isLoading) {
-		return <h1>Cargando...</h1>;
+		return (
+			<div className="center-container">
+				<h1>Cargando...</h1>
+				<div class="newtons-cradle">
+					<div class="newtons-cradle__dot"></div>
+					<div class="newtons-cradle__dot"></div>
+					<div class="newtons-cradle__dot"></div>
+					<div class="newtons-cradle__dot"></div>
+				</div>
+			</div>
+		);
 	}
-	return <div>MultipleCustomHooks</div>;
+
+	return (
+		<div className="center-container">
+			<h1>What say Albuquerque people? 🤔💎</h1>
+			<h2>Number of quotes: {counter}</h2>
+			<button className="my-button" onClick={increment}>
+				Show more quotes
+			</button>
+			<div className="card-container">
+				{data.map((quote, index) => (
+					<blockquote key={index}>
+						<p>{quote.quote}</p>
+						<figcaption>{quote.author}</figcaption>
+					</blockquote>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default MultipleCustomHooks;
--- ReactChallenges/src/main.jsx
+++ ReactChallenges/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		{/* <FirstApp value={150} /> */}
 		{/* Value será le valor por defecto del componente y al utilizar el botón de resetear, se inicializará al valor pasado como prop */}
 		{/* <FirstApp value={4} /> */}
-		<ComponentApp />
-		{/* <MultipleCustomHooks /> */}
+		{/* <ComponentApp /> */}
+		<MultipleCustomHooks />
 	</>
 );
