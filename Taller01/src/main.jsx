c03306b337c129df795ec94118c3dbedddfd8bcd
Hector Romero <hector_fabio.romero@uao.edu.co>
2023-03-05 15:05:55 -0500

    Ejercicio 5 terminado.

--- Taller01/src/main.jsx
+++ Taller01/src/main.jsx
@@ -5,12 +5,14 @@ import Exercise1 from "./pages/Exercise1";
 import Exercise2 from "./pages/Exercise2";
 import Exercise3 from "./pages/Exercise3";
 import Exercise4 from "./pages/Exercise4";
+import Exercise5 from "./pages/Exercise5";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<>
 		{/* <Exercise1 /> */}
 		{/* <Exercise2 /> */}
 		{/* <Exercise3 /> */}
-		<Exercise4 />
+		{/* <Exercise4 /> */}
+		<Exercise5 />
 	</>
 );
---
+++ Taller01/src/pages/Exercise5.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useRef } from "react";
+import Input from "../components/Input";
+import { isNumber } from "../helpers/typeValidators";
+
+const Exercise5 = () => {
+	const [orderedNumbers, setOrderedNumbers] = useState([]);
+	const numberInput = useRef();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const number = isNumber(e.target[0].value);
+		if (number === "No number" || number === "") {
+			alert("Escriba en todos los campos disponibles");
+			return;
+		}
+		const numbersArray = [...orderedNumbers];
+		numbersArray.push(number);
+		numbersArray.sort((a, b) => {
+			return a - b;
+		});
+		setOrderedNumbers(numbersArray);
+	};
+
+	return (
+		<div className="center-container">
+			<h1>Exercise 5 - Numbers</h1>
+			<form action="" onSubmit={(e) => handleSubmit(e)}>
+				<Input type="text" label="Insert a number" />
+				<button type="submit">Add</button>
+			</form>
+			<ul>
+				{orderedNumbers.map((number, index) => (
+					<li key={index}>{number}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Exercise5;
