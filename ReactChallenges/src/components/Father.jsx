6b4183153b6e5189ae925491427e261570fbe3e9
Hector Romero <hector_fabio.romero@uao.edu.co>
2023-03-14 19:47:52 -0500

    CHL09: Memorización de funciones del padre y de componentes hijos para evitar múltiples renderizados.

---
+++ ReactChallenges/src/components/Father.jsx
@@ -0,0 +1,24 @@
+import React, { useCallback, useState } from "react";
+import Son from "./Son";
+
+const Father = () => {
+	const list = [2, 4, 6, 8, 10];
+	const [valor, setValor] = useState(0);
+
+	const increment = useCallback((num) => {
+		setValor(valor + num);
+	}, []);
+
+	return (
+		<div>
+			<h1>Father</h1>
+			<p>Total: {valor}</p>
+			<hr />
+			{list.map((n, index) => {
+				return <Son key={index} numero={n} increment={increment} />;
+			})}
+		</div>
+	);
+};
+
+export default Father;
--- ReactChallenges/src/main.jsx
+++ ReactChallenges/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ComponentApp from "./ComponentApp";
+import Father from "./components/Father";
 import MultipleCustomHooks from "./components/MultipleCustomHooks";
 import FirstApp from "./FirstApp";
 import "./index.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		{/* Value será le valor por defecto del componente y al utilizar el botón de resetear, se inicializará al valor pasado como prop */}
 		{/* <FirstApp value={4} /> */}
 		{/* <ComponentApp /> */}
-		<MultipleCustomHooks />
+		{/* <MultipleCustomHooks /> */}
+		<Father />
 	</>
 );
